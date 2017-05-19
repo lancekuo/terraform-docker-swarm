cbe9a33361a8eb140e28fc9a7de478c032f80543
Lance Kuo <lancekuo@gmail.com>
2017-05-19 14:22:34 -0400

    Update remote-exec in provisioner section, add docker-machin and docker-compose cmd into tags field in the instance.

--- swarm/instance-bastion.tf
+++ swarm/instance-bastion.tf
@@ -16,8 +16,6 @@ resource "aws_instance" "swarm-bastion" {
     }
     provisioner "remote-exec" {
         inline = [
-            "sudo apt-get -y install nginx",
-            "sudo service nginx start",
             "sudo curl -L https://github.com/docker/machine/releases/download/v0.10.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && chmod +x /tmp/docker-machine && sudo cp /tmp/docker-machine /usr/local/bin/docker-machine",
             "sudo curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose",
             "sudo systemctl stop docker",
@@ -25,8 +23,10 @@ resource "aws_instance" "swarm-bastion" {
         ]
     }
     tags  {
-        Name = "${terraform.env}-swarm-bastion"
-        Env  = "${terraform.env}"
+        Name           = "${terraform.env}-swarm-bastion"
+        Env            = "${terraform.env}"
+        Docker-machine = "sudo curl -L https://github.com/docker/machine/releases/download/v0.10.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && chmod +x /tmp/docker-machine && sudo cp /tmp/docker-machine /usr/local/bin/docker-machine"
+        Docker-compose = "sudo curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose"
     }
 }
 resource "aws_eip" "swarm-bastion" {
