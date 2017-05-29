964cbea1c1a07a1d4f6558ff3e56cfada449b820
Lance Kuo <lancekuo@gmail.com>
2017-05-29 14:51:34 -0400

    Add event source, CloudWatch Scheduled, to trigger Lambda function.

--- swarm/lambda.tf
+++ swarm/lambda.tf
@@ -66,7 +66,7 @@ resource "aws_iam_role_policy_attachment" "describe-attach" {
     policy_arn = "${aws_iam_policy.ebs-backup-worker.arn}"
 }
 
-resource "aws_lambda_function" "ebs-backup" {
+resource "aws_lambda_function" "ebs-backup-create" {
     filename         = "${path.module}/lambda/createSnapshot/createSnapshot.zip"
     function_name    = "createSnapshot"
     role             = "${aws_iam_role.lambda-ebs-backup.arn}"
@@ -93,4 +93,35 @@ resource "aws_lambda_function" "ebs-backup-delete" {
             Source = "Terraform"
         }
     }
+}
+resource "aws_cloudwatch_event_rule" "every_day" {
+    name = "every-day"
+    description = "Fires every day"
+    schedule_expression = "rate(1 day)"
+}
+
+resource "aws_cloudwatch_event_target" "snapshot_every_day" {
+    rule = "${aws_cloudwatch_event_rule.every_day.name}"
+    target_id = "takeSnapshot"
+    arn = "${aws_lambda_function.ebs-backup-create.arn}"
+}
+resource "aws_cloudwatch_event_target" "cleanup_every_day" {
+    rule = "${aws_cloudwatch_event_rule.every_day.name}"
+    target_id = "cleanSnapshot"
+    arn = "${aws_lambda_function.ebs-backup-delete.arn}"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_EBSToSnapshotBackup" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.ebs-backup-create.function_name}"
+    principal = "events.amazonaws.com"
+    source_arn = "${aws_cloudwatch_event_rule.every_day.arn}"
+}
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_EBSToSnapshotCleanup" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.ebs-backup-delete.function_name}"
+    principal = "events.amazonaws.com"
+    source_arn = "${aws_cloudwatch_event_rule.every_day.arn}"
 }
