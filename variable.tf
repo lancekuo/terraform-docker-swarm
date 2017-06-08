bcc95611ed2be75390216172f5e325c9bd8823f3
Lance Kuo <lancekuo@gmail.com>
2017-06-08 14:28:46 -0400

    Separate variable from main.tf

---
+++ variable.tf
@@ -0,0 +1,41 @@
+terraform {
+    backend "s3" {
+        bucket = "terraform.internal"
+        key    = "terraform.tfstate"
+        region = "ca-central-1"
+    }
+}
+variable "region" {
+    default = "us-east-2"
+}
+
+variable "docker-ami" {
+    default = "ami-d63610b3"
+}
+
+variable "project" {
+    default = "WRS"
+}
+
+variable "bastion-key" {
+    default = {
+        "public_key_path"  = "/keys/bastion.pub"
+        "private_key_path" = "/keys/bastion"
+        "aws_key_name"     = "bastion"
+    }
+}
+variable "node-key" {
+    default = {
+        "public_key_path"  = "/keys/node.pub"
+        "private_key_path" = "/keys/node"
+        "aws_key_name"     = "node"
+    }
+}
+variable "manager-key" {
+    default = {
+        "public_key_path"  = "/keys/manager.pub"
+        "private_key_path" = "/keys/manager"
+        "aws_key_name"     = "manager"
+    }
+}
+
