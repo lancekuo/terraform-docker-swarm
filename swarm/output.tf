02231ba387d79cbcf19a1a042834fd2f0315b93e
Lance Kuo <lancekuo@gmail.com>
2017-06-08 14:26:26 -0400

    Remove square bracket in the splat variables.

--- swarm/output.tf
+++ swarm/output.tf
@@ -1,8 +1,8 @@
 output "swarm_manager" {
-    value = ["${aws_instance.swarm-manager.*.private_dns}"]
+    value = "${aws_instance.swarm-manager.*.private_dns}"
 }
 output "swarm_node" {
-    value = ["${aws_instance.swarm-node.*.private_dns}"]
+    value = "${aws_instance.swarm-node.*.private_dns}"
 }
 output "bastion_public_ip" {
     value = "${aws_eip.swarm-bastion.public_ip}"
